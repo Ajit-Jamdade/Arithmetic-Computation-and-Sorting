c979afd2a46e79c8033fee62f8b63fb03a5002ef
Ajit-Jamdade <ajitjamdade0@gmail.com>
2020-12-27 15:47:02 +0530

    Add. Added first computation

--- sortingArithmeticComputationProblem.sh
+++ sortingArithmeticComputationProblem.sh
@@ -1,9 +1,12 @@
-#! /bin/bash
+#! /bin/bash -x
 
 echo "***** Welcome to Arithmetic Computation and Sorting *****"
 
-echo "This is a program for taking three inputs from user."
+echo "This is a program of taking three inputs from user for Arithmetic computation."
+
 read -p "Enter 1st number: " a
 read -p "Enter 2nd number: " b
 read -p "Enter 3rd number: " c
+
+compute1=$(( a + b * c ))
 
