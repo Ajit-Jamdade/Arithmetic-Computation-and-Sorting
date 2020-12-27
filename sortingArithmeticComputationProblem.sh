fbc8e6c6d26e1f96632dccc5b90ef06a14252275
Ajit-Jamdade <ajitjamdade0@gmail.com>
2020-12-27 15:36:51 +0530

    Add. Added program for taking inputs from user.

--- sortingArithmeticComputationProblem.sh
+++ sortingArithmeticComputationProblem.sh
@@ -1,5 +1,9 @@
-#! /bin/bash -x
+#! /bin/bash
 
 echo "***** Welcome to Arithmetic Computation and Sorting *****"
 
+echo "This is a program for taking three inputs from user."
+read -p "Enter 1st number: " a
+read -p "Enter 2nd number: " b
+read -p "Enter 3rd number: " c
 
